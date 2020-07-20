03d7d0cb9c467bbf2ad774eefc38cc80236be9b8
snehal96km <68504146+snehal96km@users.noreply.github.com>
2020-07-20 13:05:18 +0530

    Add files via upload

---
+++ assignment1B.py
@@ -0,0 +1,14 @@
+list=[1,5,6,2,7,4]
+print(list)
+list.insert(2, 10)        #insert 10 at index 2
+print(list)
+list.remove(2)            #remove object 
+print(list)
+list.append(8)           #add object at the end of list
+print(list)
+list.sort()              # sort the list
+print(list)
+list.pop(4)              #delete the element 
+print(list) 
+ print(list[::-1])      #does not modify the original list
+list.reverse()         #reverse list
